e70fc7539429bdcf73980dbff57282d1f58a11e3
Hafsa Begum <hafsabegum0357@gmail.com>
2026-06-18 21:03:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,3 @@ resource "aws_dynamodb_table" "tasks" {
     type = "S"
   }
 }
-
-resource "aws_instance" "app" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  key_name      = "task-tracker-key"
-  tags = { Name = "task-tracker" }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.app.public_ip
-}
